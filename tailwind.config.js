d66a8b473fa2f878b1314d75cd6766f6e1240e16
Gabriel <gamcode98@gmail.com>
2022-01-20 10:35:17 -0300

    ✔ ready

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    colors: {
+      // Colors
+      // Primary
+      brightOrange: "hsl(31, 77%, 52%)",
+      darkCyan: "hsl(184, 100%, 22%)",
+      veryDarkCyan: "hsl(179, 100%, 13%)",
+      // Neutral
+      transparentWhite: "hsla(0, 0%, 100%, 0.75)" /* (paragraphs) */,
+      veryLightGray: "hsl(0, 0%, 95%)" /* (background, headings, buttons) */,
+    },
+    fontFamily: {
+      bigShouldersDisplay: "Big Shoulders Display" /* 700 */,
+      lexendDeca: "Lexend Deca" /* 400 */,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
